f105f2290422f2d36d87b3d6414979ef54893df7
Coder-Ankitkumar <155207018+Coder-Ankitkumar@users.noreply.github.com>
2026-09-16 16:20:14 +0530

    Add files via upload

--- b.js
+++ b.js
@@ -5,11 +5,46 @@
 //     window.location.href = "page2.html";
 
 // });
+// new
 
-const continueBtn = document.querySelector("#continueBtn");
+// const continueBtn = document.querySelector("#continueBtn");
 
-continueBtn.addEventListener("click", function () {
+// continueBtn.addEventListener("click", function () {
 
-    window.location.href = "and.html";
+//     window.location.href = "and.html";
+
+// });
+
+const song = document.querySelector("#song");
+const playBtn = document.querySelector("#playBtn");
+
+let isPlaying = false;
+
+playBtn.addEventListener("click", function () {
+
+    if (!isPlaying) {
+
+        song.play();
+
+        playBtn.textContent = "❚❚";
+
+        isPlaying = true;
+
+    } else {
+
+        song.pause();
+
+        playBtn.textContent = "▶";
+
+        isPlaying = false;
+    }
 
 });
+
+ const continueBtn = document.querySelector("#continueBtn");
+
+ continueBtn.addEventListener("click", function () {
+
+  window.location.href = "and.html";
+
+ });
